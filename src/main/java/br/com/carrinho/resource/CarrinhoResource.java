b030eb65319e969c32ad05000131a6dc1f68fb42
MarcosViniciusPinho <marcosjava2008@gmail.com>
2017-11-25 14:06:47 -0200

    Implementando no header o location ao criar um novo recurso de carrinho.

--- src/main/java/br/com/carrinho/resource/CarrinhoResource.java
+++ src/main/java/br/com/carrinho/resource/CarrinhoResource.java
@@ -3,15 +3,16 @@ package br.com.carrinho.resource;
 import br.com.carrinho.entity.Carrinho;
 import br.com.carrinho.service.CarrinhoService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.validation.Valid;
+import java.net.URI;
 
 @RestController
 @RequestMapping("/carrinhos")
@@ -23,7 +24,12 @@ public class CarrinhoResource {
     @PostMapping(produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE},
         consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
     public ResponseEntity<Carrinho> create(@Valid @RequestBody Carrinho carrinho){
-        return new ResponseEntity<>(this.carrinhoService.create(carrinho), HttpStatus.CREATED);
+        Carrinho carrinhoSalvo = this.carrinhoService.create(carrinho);
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}")
+                .buildAndExpand(carrinhoSalvo.getId()).toUri();
+
+        return ResponseEntity.created(uri).body(carrinhoSalvo);
     }
 
 }
