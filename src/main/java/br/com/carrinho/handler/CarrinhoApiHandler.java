ba12e7f8c1a05efb19a802f1217c08d589726dc6
MarcosViniciusPinho <marcosjava2008@gmail.com>
2017-11-26 18:05:51 -0200

    Evoluindo a API.

--- src/main/java/br/com/carrinho/handler/CarrinhoApiHandler.java
+++ src/main/java/br/com/carrinho/handler/CarrinhoApiHandler.java
@@ -1,9 +1,11 @@
 package br.com.carrinho.handler;
 
 import br.com.carrinho.handler.domain.ResponseError;
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +14,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
@@ -34,7 +37,7 @@ public class CarrinhoApiHandler extends ResponseEntityExceptionHandler {
      * @return ResponseEntity<Object>
      */
     @Override
-    protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex, HttpHeaders headers,
+    public ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex, HttpHeaders headers,
                                                                   HttpStatus status, WebRequest request) {
         List<ResponseError> erros = Arrays.asList(new ResponseError(
                 this.messageSource.getMessage("information.invalid", null, LocaleContextHolder.getLocale()),
@@ -51,10 +54,24 @@ public class CarrinhoApiHandler extends ResponseEntityExceptionHandler {
      * @return ResponseEntity<Object>
      */
     @Override
-    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers,
+    public ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers,
                                                                   HttpStatus status, WebRequest request) {
-        List<ResponseError> erros = createErros(ex.getBindingResult());
+        List<ResponseError> erros = this.createErros(ex.getBindingResult());
         return handleExceptionInternal(ex, erros, headers, HttpStatus.BAD_REQUEST, request);
+    }
+
+    /**
+     * Método que gera uma exceção quando o atributo id de um relacionamento nao existir na entidade em questão.
+     * @param ex ex
+     * @param request request
+     * @return ResponseEntity<Object>
+     */
+    @ExceptionHandler({ DataIntegrityViolationException.class } )
+    public ResponseEntity<Object> handleDataIntegrityViolationException(DataIntegrityViolationException ex, WebRequest request) {
+        List<ResponseError> erros = Arrays.asList(new ResponseError(
+                this.messageSource.getMessage("operation.failed", null, LocaleContextHolder.getLocale()),
+                ExceptionUtils.getRootCauseMessage(ex)));
+        return handleExceptionInternal(ex, erros, new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
     }
 
     private List<ResponseError> createErros(BindingResult bindingResult) {
