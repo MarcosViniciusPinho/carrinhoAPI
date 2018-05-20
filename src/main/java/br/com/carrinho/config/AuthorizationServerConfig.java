0dd8f7b82c02cb88f7683c48e3f254f37d1080e5
MarcosViniciusPinho <marcosjava2008@gmail.com>
2018-05-20 20:33:06 -0300

    Alteração da assinatura do acess token.

--- src/main/java/br/com/carrinho/config/AuthorizationServerConfig.java
+++ src/main/java/br/com/carrinho/config/AuthorizationServerConfig.java
@@ -24,7 +24,7 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
         clients.inMemory()
                 .withClient("angular")
-                .secret("svb")
+                .secret("svc")
                 .scopes("read", "write")
                 .authorizedGrantTypes("password", "refresh_token")
                 .accessTokenValiditySeconds(1800)
